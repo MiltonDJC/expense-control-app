c6f3fea6c551e45276249a6effe347dd4f13d64b
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-19 02:39:45 -0300

    refactor: move app file

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:expense_control_app/features/home/presentation/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomeScreen());
+  }
+}
