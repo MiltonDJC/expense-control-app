7bd89f0e6e2e2493ac7dffe2a8b4a099a90d6288
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-19 15:24:38 -0300

    app: add safe area widget and set in false the debugShowCheckedBanner

--- lib/app.dart
+++ lib/app.dart
@@ -6,6 +6,9 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomeScreen());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(child: HomeScreen()),
+    );
   }
 }
