eeaef5724e2d18f3bf83e38ecf5370357bcceea4
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-21 04:58:35 -0300

    feat(main): add initialization method

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:expense_control_app/app.dart';
+import 'package:expense_control_app/config/initialization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  Initialization.init();
   runApp(const ProviderScope(child: App()));
 }
