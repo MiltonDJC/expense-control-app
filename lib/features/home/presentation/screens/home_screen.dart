314b186b2c669de481cad85c49f988f6ececa2ff
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-25 16:56:05 -0300

    feat(expenses): update home screen adding ExpensesScreen widget

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:expense_control_app/features/balance/presentation/screens/balance_screen.dart';
+import 'package:expense_control_app/features/expenses/presentation/screens/expenses_screen.dart';
 import 'package:expense_control_app/features/home/presentation/widgets/home_navigator_side_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -13,9 +14,8 @@ class _HomeScreenState extends State<HomeScreen> {
   final screens = <Widget>[
     const BalanceScreen(),
     const Center(child: Text('Money Pockets Screen')),
-    const Center(child: Text('Expenses Screen')),
+    const ExpensesScreen(),
     const Center(child: Text('Fixed Term Deposit Screen')),
-    // ExpensesScreen(),
     // FixedTermDeposit(),
     // MoneyPockets(),
   ];
