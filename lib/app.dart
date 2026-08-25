59dc9d43029cc22c6aaa85c80b45cdac024f984d
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-25 09:51:28 -0300

    fix(app): remove safe area widget

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeArea(child: HomeScreen()),
+      home: HomeScreen(),
     );
   }
 }
