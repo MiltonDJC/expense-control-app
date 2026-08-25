0590439f1ff23cf595c6e54838bc98d63b4e99c2
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-25 13:26:36 -0300

    feat(config): update main adding both database initalization and provider scope override

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:expense_control_app/app.dart';
 import 'package:expense_control_app/config/initialization.dart';
+import 'package:expense_control_app/core/providers/database_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  Initialization.init();
-  runApp(const ProviderScope(child: App()));
+void main() async {
+  final appDatabase = await Initialization.init();
+
+  runApp(
+    ProviderScope(
+      overrides: [appDatabaseProvider.overrideWithValue(appDatabase)],
+      child: const App(),
+    ),
+  );
 }
