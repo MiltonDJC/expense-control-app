3de4434e62fc55f8169fff2bb334f4dc41f71caf
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-25 16:49:24 -0300

    refactor(config): comment mock data

--- lib/config/initialization.dart
+++ lib/config/initialization.dart
@@ -1,10 +1,21 @@
+// import 'package:drift/drift.dart';
 import 'package:expense_control_app/core/database/app_database.dart';
+// import 'package:expense_control_app/features/expenses/domain/enums/pay_method.dart';
 import 'package:flutter/material.dart';
 
 class Initialization {
   static Future<AppDatabase> init() async {
     WidgetsFlutterBinding.ensureInitialized();
     final database = AppDatabase();
+    // database
+    //     .into(database.expense)
+    //     .insert(
+    //       const ExpenseCompanion(
+    //         name: Value('Gasto de prueba'),
+    //         payMethod: Value(PayMethod.cash),
+    //         isFixed: Value(false),
+    //       ),
+    //     );
     return database;
   }
 }
