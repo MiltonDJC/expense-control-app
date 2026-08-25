391a10f1020d84b185caf1faca62e0ac17bd1704
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-25 13:29:51 -0300

    feat(expenses): add expense local datasource

---
+++ lib/features/expenses/data/datasources/expenses_local_data_source.dart
@@ -0,0 +1,16 @@
+import 'package:expense_control_app/core/database/app_database.dart';
+import 'package:expense_control_app/features/expenses/data/models/expense_model.dart';
+
+class ExpensesLocalDataSource {
+  ExpensesLocalDataSource({required this.appDatabase});
+
+  final AppDatabase appDatabase;
+
+  Future<List<ExpenseModel>> getAllExpenses() async {
+    final expensesRows = await appDatabase.managers.expense.get();
+    final expensesModels = expensesRows
+        .map((expenseRow) => ExpenseModel.fromDrift(expenseRow))
+        .toList();
+    return expensesModels;
+  }
+}
