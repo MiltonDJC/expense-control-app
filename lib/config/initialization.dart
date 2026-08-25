a4b523d5132ebcf2a28db7604ec8861b798fb3e9
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-25 13:24:42 -0300

    feat(config): update Initialization class with database initialization

--- lib/config/initialization.dart
+++ lib/config/initialization.dart
@@ -1,7 +1,10 @@
+import 'package:expense_control_app/core/database/app_database.dart';
 import 'package:flutter/material.dart';
 
 class Initialization {
-  static void init() {
+  static Future<AppDatabase> init() async {
     WidgetsFlutterBinding.ensureInitialized();
+    final database = AppDatabase();
+    return database;
   }
 }
