182ad445b1f3fa0c2db1f4c6407c4a572479b8c0
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-25 13:29:17 -0300

    feat(expenses): add expenses repository impl methods

---
+++ lib/features/expenses/data/repositories/expenses_repository_impl.dart
@@ -0,0 +1,32 @@
+import 'package:expense_control_app/features/expenses/data/datasources/expenses_local_data_source.dart';
+import 'package:expense_control_app/features/expenses/domain/entities/expense_entity.dart';
+import 'package:expense_control_app/features/expenses/domain/repositories/expenses_repository.dart';
+
+class ExpensesRepositoryImpl implements ExpensesRepository {
+  ExpensesRepositoryImpl({required this.dataSource});
+
+  final ExpensesLocalDataSource dataSource;
+
+  @override
+  Future<List<ExpenseEntity>> getAllExpenses() async {
+    final expensesModels = await dataSource.getAllExpenses();
+    return expensesModels
+        .map((expenseModel) => expenseModel.toEntity())
+        .toList();
+  }
+
+  @override
+  Future<void> deleteExpense({required int id}) {
+    // TODO: implement deleteExpense
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateExpense({
+    required int id,
+    required ExpenseEntity newExpense,
+  }) {
+    // TODO: implement updateExpense
+    throw UnimplementedError();
+  }
+}
