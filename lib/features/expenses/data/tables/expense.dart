b4bb68d86811e2590d012a3190f48e754a4e46e0
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-25 10:25:42 -0300

    feat(expenses): update expense table

--- lib/features/expenses/data/tables/expense.dart
+++ lib/features/expenses/data/tables/expense.dart
@@ -1,9 +1,11 @@
 import 'package:drift/drift.dart';
+import 'package:expense_control_app/features/expenses/domain/enums/bank.dart';
 import 'package:expense_control_app/features/expenses/domain/enums/pay_method.dart';
 
 class Expense extends Table {
   late final id = integer().autoIncrement()();
   late final name = text()();
   late final payMethod = intEnum<PayMethod>()();
+  late final bank = intEnum<Bank>().nullable()();
   late final isFixed = boolean()();
 }
