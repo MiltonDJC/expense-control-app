a17f98b7e4eb4d65aa0ddbfdfdcd0ee9f603e8ee
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-26 04:14:16 -0300

    feat(config): add mock data for initializing app database

--- lib/config/initialization.dart
+++ lib/config/initialization.dart
@@ -1,21 +1,32 @@
-// import 'package:drift/drift.dart';
+import 'package:drift/drift.dart';
 import 'package:expense_control_app/core/database/app_database.dart';
-// import 'package:expense_control_app/features/expenses/domain/enums/pay_method.dart';
+import 'package:expense_control_app/features/expenses/domain/enums/bank.dart';
+import 'package:expense_control_app/features/expenses/domain/enums/pay_method.dart';
 import 'package:flutter/material.dart';
 
 class Initialization {
   static Future<AppDatabase> init() async {
     WidgetsFlutterBinding.ensureInitialized();
     final database = AppDatabase();
-    // database
-    //     .into(database.expense)
-    //     .insert(
-    //       const ExpenseCompanion(
-    //         name: Value('Gasto de prueba'),
-    //         payMethod: Value(PayMethod.cash),
-    //         isFixed: Value(false),
-    //       ),
-    //     );
+    database
+        .into(database.expense)
+        .insert(
+          const ExpenseCompanion(
+            name: Value('Gasto de prueba 1'),
+            payMethod: Value(PayMethod.cash),
+            isFixed: Value(false),
+          ),
+        );
+    database
+        .into(database.expense)
+        .insert(
+          const ExpenseCompanion(
+            name: Value('Gasto de prueba 2'),
+            bank: Value(Bank.bancoComafi),
+            payMethod: Value(PayMethod.cash),
+            isFixed: Value(true),
+          ),
+        );
     return database;
   }
 }
