09553af7085051c6fbfe12527e6e56c878df5627
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-26 05:11:45 -0300

    feat(expenses): update expenses screen

--- lib/features/expenses/presentation/screens/expenses_screen.dart
+++ lib/features/expenses/presentation/screens/expenses_screen.dart
@@ -1,4 +1,6 @@
+import 'package:expense_control_app/features/expenses/presentation/providers/expenses_use_cases_provider.dart';
 import 'package:expense_control_app/features/expenses/presentation/state/expenses_notifier.dart';
+import 'package:expense_control_app/features/expenses/presentation/widgets/expense_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -22,27 +24,17 @@ class ExpensesScreen extends ConsumerWidget {
                 itemCount: state.expenses.length,
                 itemBuilder: (context, index) {
                   return Center(
-                    child: Card(
-                      child: Column(
-                        children: [
-                          Text(state.expenses[index].id.toString()),
-                          Text(state.expenses[index].name),
-                          if (state.expenses[index].bank != null)
-                            Text(state.expenses[index].bank!.name),
-                          if (state.expenses[index].isFixed)
-                            const Text('Gasto fijo'),
-                          Text(state.expenses[index].payMethod.name),
-                          ElevatedButton.icon(
-                            onPressed: () async {
-                              await ref
-                                  .read(expensesProvider.notifier)
-                                  .deleteExpense(id: state.expenses[index].id);
-                              ref.invalidate(expensesProvider);
-                            },
-                            label: const Icon(Icons.delete),
-                          ),
-                        ],
-                      ),
+                    child: ExpenseWidget(
+                      expenseName: state.expenses[index].name,
+                      bankName: state.expenses[index].bank?.name,
+                      isFixed: state.expenses[index].isFixed,
+                      payMethod: state.expenses[index].payMethod.name,
+                      onPressed: () async {
+                        await ref.read(deleteExpenseUseCaseProvider)(
+                          id: state.expenses[index].id,
+                        );
+                        ref.invalidate(expensesProvider);
+                      },
                     ),
                   );
                 },
