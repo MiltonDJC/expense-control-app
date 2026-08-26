df792c0fdd49477ef72bd87272b6cd0e27268b2b
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-26 04:19:09 -0300

    feat(expenses): update expenses screen

--- lib/features/expenses/presentation/screens/expenses_screen.dart
+++ lib/features/expenses/presentation/screens/expenses_screen.dart
@@ -11,15 +11,50 @@ class ExpensesScreen extends ConsumerWidget {
 
     return Scaffold(
       body: expenses.when(
-        data: (state) => ListView.builder(
-          itemCount: state.expenses.length,
-          itemBuilder: (context, index) {
-            return Center(child: Card(child: Text(state.expenses[index].name)));
-          },
-        ),
+        data: (state) => state.expenses.isEmpty
+            ? const Center(
+                child: Text(
+                  'No hay gastos registrados por el momento...',
+                  style: TextStyle(fontSize: 26),
+                ),
+              )
+            : ListView.builder(
+                itemCount: state.expenses.length,
+                itemBuilder: (context, index) {
+                  return Center(
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Text(state.expenses[index].id.toString()),
+                          Text(state.expenses[index].name),
+                          if (state.expenses[index].bank != null)
+                            Text(state.expenses[index].bank!.name),
+                          if (state.expenses[index].isFixed)
+                            const Text('Gasto fijo'),
+                          Text(state.expenses[index].payMethod.name),
+                          ElevatedButton.icon(
+                            onPressed: () async {
+                              await ref
+                                  .read(expensesProvider.notifier)
+                                  .deleteExpense(id: state.expenses[index].id);
+                              ref.invalidate(expensesProvider);
+                            },
+                            label: const Icon(Icons.delete),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
         error: (error, stackTrace) =>
             Center(child: Text('Ha ocurrido un error: ${error.toString()}')),
         loading: () => const Center(child: CircularProgressIndicator()),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Agregar nuevo gasto',
+        child: const Icon(Icons.add, size: 32),
       ),
     );
   }
