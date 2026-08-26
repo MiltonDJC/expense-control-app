d068b6048a07c1df5f7b5dd4b46a28afb644dd75
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-26 04:16:55 -0300

    feat(expenses): add delete expense use case provider

--- lib/features/expenses/presentation/providers/expenses_use_cases_provider.dart
+++ lib/features/expenses/presentation/providers/expenses_use_cases_provider.dart
@@ -1,3 +1,4 @@
+import 'package:expense_control_app/features/expenses/domain/use_cases/delete_expense_use_case.dart';
 import 'package:expense_control_app/features/expenses/domain/use_cases/get_all_expenses_use_case.dart';
 import 'package:expense_control_app/features/expenses/presentation/providers/expenses_repository_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -7,4 +8,10 @@ part 'expenses_use_cases_provider.g.dart';
 GetAllExpensesUseCase getAllExpensesUseCase(Ref ref) {
   final repository = ref.read(expensesRepositoryProvider);
   return GetAllExpensesUseCase(expensesRepository: repository);
+}
+
+@riverpod
+DeleteExpenseUseCase deleteExpenseUseCase(Ref ref) {
+  final repository = ref.read(expensesRepositoryProvider);
+  return DeleteExpenseUseCase(expensesRepository: repository);
 }
