557130a43d24842c92638763cc09537985f7a334
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-27 09:11:07 -0300

    feat(expenses): add update expense method in expenses notifier

--- lib/features/expenses/presentation/state/expenses_notifier.dart
+++ lib/features/expenses/presentation/state/expenses_notifier.dart
@@ -1,4 +1,6 @@
 import 'package:expense_control_app/features/expenses/domain/entities/expense_entity.dart';
+import 'package:expense_control_app/features/expenses/domain/enums/bank.dart';
+import 'package:expense_control_app/features/expenses/domain/enums/pay_method.dart';
 import 'package:expense_control_app/features/expenses/presentation/providers/expenses_use_cases_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -22,5 +24,21 @@ class ExpensesNotifier extends _$ExpensesNotifier {
 
   Future<void> deleteExpense({required int id}) async {
     await ref.read(deleteExpenseUseCaseProvider)(id: id);
+  }
+
+  Future<void> updateExpense({
+    required int id,
+    String? name,
+    PayMethod? payMethod,
+    Bank? bank,
+    bool? isFixed,
+  }) async {
+    await ref.read(updateExpenseUseCaseProvider)(
+      id: id,
+      name: name,
+      payMethod: payMethod,
+      bank: bank,
+      isFixed: isFixed,
+    );
   }
 }
