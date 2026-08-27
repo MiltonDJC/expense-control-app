1eec8613dfd5b00588370c790adeaac9045b4602
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-27 15:44:57 -0300

    feat(expenses): add the add expense use case

---
+++ lib/features/expenses/domain/use_cases/add_expense_use_case.dart
@@ -0,0 +1,21 @@
+import 'package:expense_control_app/features/expenses/domain/enums/bank.dart';
+import 'package:expense_control_app/features/expenses/domain/enums/pay_method.dart';
+import 'package:expense_control_app/features/expenses/domain/repositories/expenses_repository.dart';
+
+class AddExpenseUseCase {
+  AddExpenseUseCase({required this.expensesRepository});
+
+  final ExpensesRepository expensesRepository;
+
+  Future<void> call({
+    required String name,
+    required PayMethod payMethod,
+    Bank? bank,
+    required bool isFixed,
+  }) => expensesRepository.addExpense(
+    name: name,
+    payMethod: payMethod,
+    bank: bank,
+    isFixed: isFixed,
+  );
+}
