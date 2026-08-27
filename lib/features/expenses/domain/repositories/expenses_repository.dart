385f5909cdc85151e0b7855230df8e0f122e8054
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-27 07:22:08 -0300

    fix(expenses): update updateExpense method

--- lib/features/expenses/domain/repositories/expenses_repository.dart
+++ lib/features/expenses/domain/repositories/expenses_repository.dart
@@ -1,10 +1,15 @@
 import 'package:expense_control_app/features/expenses/domain/entities/expense_entity.dart';
+import 'package:expense_control_app/features/expenses/domain/enums/bank.dart';
+import 'package:expense_control_app/features/expenses/domain/enums/pay_method.dart';
 
 abstract class ExpensesRepository {
   Future<List<ExpenseEntity>> getAllExpenses();
   Future<void> deleteExpense({required int id});
   Future<void> updateExpense({
     required int id,
-    required ExpenseEntity newExpense,
+    String? name,
+    PayMethod? payMethod,
+    Bank? bank,
+    bool? isFixed,
   });
 }
