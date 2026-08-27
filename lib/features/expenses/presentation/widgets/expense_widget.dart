e92194d20ea41885bc028d1e09ae25fbaa4b9289
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-27 10:26:16 -0300

    feat(expenses): update ExpenseWidget

--- lib/features/expenses/presentation/widgets/expense_widget.dart
+++ lib/features/expenses/presentation/widgets/expense_widget.dart
@@ -23,21 +23,73 @@ class ExpenseWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [
-          Row(children: [Text(expenseName)]),
-          if (bankName != null) Text(getBankName(bankName!)),
-          if (isFixed) const Text('Gasto Fijo'),
-          Text(getPayMethodName(payMethod)),
-          ElevatedButton.icon(
-            onPressed: onEdited,
-            label: const Icon(Icons.edit),
-          ),
-          ElevatedButton.icon(
-            onPressed: onDeleted,
-            label: const Icon(Icons.delete),
-          ),
-        ],
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          spacing: 10,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  expenseName,
+                  style: const TextStyle(fontSize: 26, fontWeight: .w600),
+                ),
+                if (isFixed)
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: const Text(
+                      'Gasto Fijo',
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    ),
+                  ),
+              ],
+            ),
+            Row(
+              children: [
+                const Text('Pagado con: ', style: TextStyle(fontSize: 20)),
+                Text(
+                  getPayMethodName(payMethod),
+                  style: const TextStyle(fontSize: 20),
+                ),
+                if (bankName != null)
+                  Text(
+                    ' (${getBankName(bankName!)})',
+                    style: const TextStyle(fontSize: 20),
+                  ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              spacing: 12,
+              children: [
+                OutlinedButton.icon(
+                  onPressed: onEdited,
+                  icon: const Icon(
+                    Icons.edit,
+                    color: Color(0xFF2F3776),
+                    size: 22,
+                  ),
+                  label: const Text('Editar', style: TextStyle(fontSize: 22)),
+                ),
+                OutlinedButton.icon(
+                  onPressed: onDeleted,
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Color(0xFFFF0033),
+                    size: 22,
+                  ),
+                  label: const Text('Eliminar', style: TextStyle(fontSize: 22)),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
