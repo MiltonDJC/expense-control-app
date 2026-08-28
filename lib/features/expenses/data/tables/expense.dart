4de8e9d1fa0ed38c76ace6150a4e97bc3934afc7
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-28 06:46:04 -0300

    feat(expenses): add amount column in expense table

--- lib/features/expenses/data/tables/expense.dart
+++ lib/features/expenses/data/tables/expense.dart
@@ -5,6 +5,7 @@ import 'package:expense_control_app/features/expenses/domain/enums/pay_method.da
 class Expense extends Table {
   late final id = integer().autoIncrement()();
   late final name = text()();
+  late final amount = real()();
   late final payMethod = intEnum<PayMethod>()();
   late final bank = intEnum<Bank>().nullable()();
   late final isFixed = boolean()();
