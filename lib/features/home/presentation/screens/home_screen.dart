5924e6d64507573d22491cab6e9bcc6aa044771a
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-28 11:33:50 -0300

    feat(home): update home screen

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:expense_control_app/features/balance/presentation/screens/balance_screen.dart';
 import 'package:expense_control_app/features/expenses/presentation/screens/expenses_screen.dart';
 import 'package:expense_control_app/features/fixed_term_deposit/presentation/screens/fixed_term_deposit_screen.dart';
-import 'package:expense_control_app/features/home/presentation/widgets/home_navigator_side_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -25,14 +24,44 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          HomeNavigatorSideBar(
-            onChanged: (newSelectedIndex) =>
-                setState(() => selectedIndex = newSelectedIndex),
+      body: screens[selectedIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: selectedIndex,
+        onDestinationSelected: (value) => setState(() => selectedIndex = value),
+        indicatorColor: Colors.transparent,
+        // indicatorShape: const OutlineInputBorder(
+        // borderSide: BorderSide(color: Colors.transparent),
+        // borderRadius: BorderRadius.all(.circular(10)),
+        // ),
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+        labelTextStyle: const WidgetStatePropertyAll(
+          TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+        ),
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.account_balance_wallet_outlined),
+            selectedIcon: Icon(Icons.account_balance_wallet),
+            label: 'Balance',
+            tooltip: 'Mi balance',
           ),
-          const VerticalDivider(),
-          Expanded(child: screens[selectedIndex]),
+          NavigationDestination(
+            icon: Icon(Icons.savings_outlined),
+            selectedIcon: Icon(Icons.savings),
+            label: 'Bolsillos',
+            tooltip: 'Mis bolsillos',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.money_off_csred_outlined),
+            selectedIcon: Icon(Icons.money_off_csred),
+            label: 'Gastos',
+            tooltip: 'Mis gastos',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.account_balance_outlined),
+            selectedIcon: Icon(Icons.account_balance),
+            label: 'Plazos Fijos',
+            tooltip: 'Mis plazos fijos',
+          ),
         ],
       ),
     );
