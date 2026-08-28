681f442645cda90f79ef349ecd50f3e9aee9429e
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-28 01:23:19 -0300

    fix(fixed_term_deposit): fix deposit due date parameter name

--- lib/features/fixed_term_deposit/data/table/fixed_term_deposit.dart
+++ lib/features/fixed_term_deposit/data/table/fixed_term_deposit.dart
@@ -4,7 +4,7 @@ class FixedTermDeposit extends Table {
   late final id = integer().autoIncrement()();
   late final depositAmount = real()();
   late final depositDate = dateTime()();
-  late final dueDepositDate = dateTime()();
+  late final depositDueDate = dateTime()();
   late final dolarPrice = real()();
   late final name = text()();
 }
