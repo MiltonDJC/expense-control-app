ca88d4f2f4da19abb5e3a3a5d5755168b7f2e1e0
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-28 04:21:17 -0300

    feat(fixed_term_deposit): add fixed term deposit repository implementation methods

---
+++ lib/features/fixed_term_deposit/data/repositories/fixed_term_deposit_repository_impl.dart
@@ -0,0 +1,58 @@
+import 'package:expense_control_app/features/fixed_term_deposit/data/datasources/fixed_term_deposit_local_data_source.dart';
+import 'package:expense_control_app/features/fixed_term_deposit/domain/entities/fixed_term_deposit_entity.dart';
+import 'package:expense_control_app/features/fixed_term_deposit/domain/repositories/fixed_term_deposit_repository.dart';
+
+class FixedTermDepositRepositoryImpl implements FixedTermDepositRepository {
+  const FixedTermDepositRepositoryImpl({required this.dataSource});
+
+  final FixedTermDepositLocalDataSource dataSource;
+
+  @override
+  Future<void> addFixedTermDeposit({
+    required double depositAmount,
+    required DateTime depositDate,
+    required DateTime depositDueDate,
+    required double dolarPrice,
+    required String name,
+  }) async {
+    await dataSource.addFixedTermDeposit(
+      depositAmount: depositAmount,
+      depositDate: depositDate,
+      depositDueDate: depositDueDate,
+      dolarPrice: dolarPrice,
+      name: name,
+    );
+  }
+
+  @override
+  Future<void> deleteFixedTermDeposit({required int id}) async {
+    await dataSource.deleteFixedTermDeposit(id: id);
+  }
+
+  @override
+  Future<void> updateFixedTermDeposit({
+    required int id,
+    double? depositAmount,
+    DateTime? depositDate,
+    DateTime? depositDueDate,
+    double? dolarPrice,
+    String? name,
+  }) async {
+    await dataSource.updateFixedTermDeposit(
+      id: id,
+      depositAmount: depositAmount,
+      depositDate: depositDate,
+      depositDueDate: depositDueDate,
+      dolarPrice: dolarPrice,
+      name: name,
+    );
+  }
+
+  @override
+  Future<List<FixedTermDepositEntity>> getAllFixedTermDeposits() async {
+    final fixedTermDepositsModels = await dataSource.getAllFixedTermDeposits();
+    return fixedTermDepositsModels
+        .map((fixedTermDepositsModel) => fixedTermDepositsModel.toEntity())
+        .toList();
+  }
+}
