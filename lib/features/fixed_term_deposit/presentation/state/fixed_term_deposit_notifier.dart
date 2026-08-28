2df7d1d8a039fe566a498c259b8089ff78f3bb23
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-28 04:22:31 -0300

    feat(fixed_term_deposit): add both fixed term deposit notifier and state

---
+++ lib/features/fixed_term_deposit/presentation/state/fixed_term_deposit_notifier.dart
@@ -0,0 +1,62 @@
+import 'package:expense_control_app/features/fixed_term_deposit/domain/entities/fixed_term_deposit_entity.dart';
+import 'package:expense_control_app/features/fixed_term_deposit/presentation/providers/fixed_term_deposit_use_cases_provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'fixed_term_deposit_notifier.freezed.dart';
+part 'fixed_term_deposit_notifier.g.dart';
+
+@freezed
+abstract class FixedTermDepositState with _$FixedTermDepositState {
+  const factory FixedTermDepositState({
+    @Default([]) List<FixedTermDepositEntity> fixedTermDeposits,
+  }) = _FixedTermDepositState;
+}
+
+@riverpod
+class FixedTermDepositNotifier extends _$FixedTermDepositNotifier {
+  @override
+  Future<FixedTermDepositState> build() async {
+    final fixedTermDeposits = await ref.read(
+      getAllFixedTermDepositsUseCaseProvider,
+    )();
+    return FixedTermDepositState(fixedTermDeposits: fixedTermDeposits);
+  }
+
+  Future<void> addFixedTermDeposit({
+    required double depositAmount,
+    required DateTime depositDate,
+    required DateTime depositDueDate,
+    required double dolarPrice,
+    required String name,
+  }) async {
+    await ref.read(addFixedTermDepositUseCaseProvider)(
+      depositAmount: depositAmount,
+      depositDate: depositDate,
+      depositDueDate: depositDueDate,
+      dolarPrice: dolarPrice,
+      name: name,
+    );
+  }
+
+  Future<void> updateFixedTermDeposit({
+    required int id,
+    double? depositAmount,
+    DateTime? depositDate,
+    DateTime? depositDueDate,
+    double? dolarPrice,
+    String? name,
+  }) async {
+    await ref.read(updateExpenseUseCaseProvider)(
+      id: id,
+      depositAmount: depositAmount,
+      depositDate: depositDate,
+      depositDueDate: depositDueDate,
+      dolarPrice: dolarPrice,
+      name: name,
+    );
+  }
+
+  Future<void> deleteFixedTermDeposit({required int id}) async {
+    await ref.read(deleteFixedTermDepositUseCaseProvider)(id: id);
+  }
+}
