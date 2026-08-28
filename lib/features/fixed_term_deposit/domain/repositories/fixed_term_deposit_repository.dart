efc96146d5ae4cfbf385eefe9a5462f50bf16f88
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-28 01:33:01 -0300

    feat(fixed_term_deposit): update fixed term deposit repository contracts

--- lib/features/fixed_term_deposit/domain/repositories/fixed_term_deposit_repository.dart
+++ lib/features/fixed_term_deposit/domain/repositories/fixed_term_deposit_repository.dart
@@ -1,8 +1,21 @@
 import 'package:expense_control_app/features/fixed_term_deposit/domain/entities/fixed_term_deposit_entity.dart';
 
 abstract class FixedTermDepositRepository {
-  Future<void> addFixedTermDeposit();
-  Future<void> updateFixedTermDeposit();
-  Future<void> deleteFixedTermDeposit();
+  Future<void> addFixedTermDeposit({
+    required double depositAmount,
+    required DateTime depositDate,
+    required DateTime depositDueDate,
+    required double dolarPrice,
+    required String name,
+  });
+  Future<void> updateFixedTermDeposit({
+    required int id,
+    double? depositAmount,
+    DateTime? depositDate,
+    DateTime? depositDueDate,
+    double? dolarPrice,
+    String? name,
+  });
+  Future<void> deleteFixedTermDeposit({required int id});
   Future<List<FixedTermDepositEntity>> getAllFixedTermDeposits();
 }
