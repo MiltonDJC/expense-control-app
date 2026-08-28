4cc08bc723de4b0b797df02185d0f20eef8c1b46
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-28 01:24:06 -0300

    feat(fixed_term_deposit): add fixed term deposit entity

---
+++ lib/features/fixed_term_deposit/domain/entities/fixed_term_deposit_entity.dart
@@ -0,0 +1,17 @@
+class FixedTermDepositEntity {
+  FixedTermDepositEntity({
+    required this.id,
+    required this.depositAmount,
+    required this.depositDate,
+    required this.depositDueDate,
+    required this.dolarPrice,
+    required this.name,
+  });
+
+  final int id;
+  final double depositAmount;
+  final DateTime depositDate;
+  final DateTime depositDueDate;
+  final double dolarPrice;
+  final String name;
+}
