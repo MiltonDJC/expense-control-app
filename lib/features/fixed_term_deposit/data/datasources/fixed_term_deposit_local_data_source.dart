38563d00bc35474bdb13da9a562c5a13ed6b95da
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-28 04:20:05 -0300

    feat(fixed_term_deposit): add fixed term deposit local datasource with its methods

---
+++ lib/features/fixed_term_deposit/data/datasources/fixed_term_deposit_local_data_source.dart
@@ -0,0 +1,66 @@
+import 'package:drift/drift.dart';
+import 'package:expense_control_app/core/database/app_database.dart';
+import 'package:expense_control_app/features/fixed_term_deposit/data/models/fixed_term_deposit_model.dart';
+
+class FixedTermDepositLocalDataSource {
+  FixedTermDepositLocalDataSource({required this.appDatabase});
+
+  final AppDatabase appDatabase;
+
+  Future<void> addFixedTermDeposit({
+    required double depositAmount,
+    required DateTime depositDate,
+    required DateTime depositDueDate,
+    required double dolarPrice,
+    required String name,
+  }) async {
+    await appDatabase.managers.fixedTermDeposit.create(
+      (o) => o(
+        depositAmount: depositAmount,
+        depositDate: depositDate,
+        depositDueDate: depositDueDate,
+        dolarPrice: dolarPrice,
+        name: name,
+      ),
+    );
+  }
+
+  Future<void> updateFixedTermDeposit({
+    required int id,
+    double? depositAmount,
+    DateTime? depositDate,
+    DateTime? depositDueDate,
+    double? dolarPrice,
+    String? name,
+  }) async {
+    await appDatabase.managers.fixedTermDeposit
+        .filter((f) => f.id(id))
+        .update(
+          (o) => o(
+            depositAmount: Value.absentIfNull(depositAmount),
+            depositDate: Value.absentIfNull(depositDate),
+            depositDueDate: Value.absentIfNull(depositDueDate),
+            dolarPrice: Value.absentIfNull(dolarPrice),
+            name: Value.absentIfNull(name),
+          ),
+        );
+  }
+
+  Future<void> deleteFixedTermDeposit({required int id}) async {
+    await appDatabase.managers.fixedTermDeposit
+        .filter((f) => f.id(id))
+        .delete();
+  }
+
+  Future<List<FixedTermDepositModel>> getAllFixedTermDeposits() async {
+    final fixedTermDepositRows = await appDatabase.managers.fixedTermDeposit
+        .get();
+    final fixedTermDepositModels = fixedTermDepositRows
+        .map(
+          (fixedTermDepositRow) =>
+              FixedTermDepositModel.fromDrift(fixedTermDepositRow),
+        )
+        .toList();
+    return fixedTermDepositModels;
+  }
+}
