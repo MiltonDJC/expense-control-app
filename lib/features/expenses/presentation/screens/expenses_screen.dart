208d594bbfc44285dcac8635aa0757d8b767a986
MiltonDJC <miltonaxe2001@gmail.com>
2026-08-31 20:18:37 -0300

    feat(expenses): update expenses screen

--- lib/features/expenses/presentation/screens/expenses_screen.dart
+++ lib/features/expenses/presentation/screens/expenses_screen.dart
@@ -4,6 +4,7 @@ import 'package:expense_control_app/features/expenses/presentation/state/expense
 import 'package:expense_control_app/features/expenses/presentation/widgets/expense_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ExpensesScreen extends ConsumerWidget {
   const new({super.key});
@@ -24,30 +25,58 @@ class ExpensesScreen extends ConsumerWidget {
             : ListView.builder(
                 itemCount: state.expenses.length,
                 itemBuilder: (context, index) {
-                  return ExpenseWidget(
-                    expenseName: state.expenses[index].name,
-                    amount: state.expenses[index].amount,
-                    bankName: state.expenses[index].bank?.name,
-                    isFixed: state.expenses[index].isFixed,
-                    payMethod: state.expenses[index].payMethod.name,
-                    onEdited: () async {
-                      await ref
-                          .read(expensesProvider.notifier)
-                          .updateExpense(
-                            id: state.expenses[index].id,
-                            name: 'Gasto actualizado',
-                            bank: Bank.bancoProvincia,
-                            isFixed: true,
-                            payMethod: PayMethod.mercadoPago,
-                          );
-                      ref.invalidate(expensesProvider);
-                    },
-                    onDeleted: () async {
-                      await ref
-                          .read(expensesProvider.notifier)
-                          .deleteExpense(id: state.expenses[index].id);
-                      ref.invalidate(expensesProvider);
-                    },
+                  return Slidable(
+                    key: Key(state.expenses[index].id.toString()),
+                    startActionPane: ActionPane(
+                      motion: const ScrollMotion(),
+                      extentRatio: 0.2,
+                      children: [
+                        SlidableAction(
+                          onPressed: (context) async {
+                            await ref
+                                .read(expensesProvider.notifier)
+                                .updateExpense(
+                                  id: state.expenses[index].id,
+                                  amount: 6,
+                                  bank: Bank.bancoComafi,
+                                  isFixed: true,
+                                  payMethod: PayMethod.creditCard,
+                                  name: 'Depósito actualizado',
+                                );
+                            ref.invalidate(expensesProvider);
+                          },
+                          backgroundColor: Colors.blue,
+                          foregroundColor: Colors.white,
+                          icon: Icons.edit,
+                          label: 'Editar',
+                        ),
+                      ],
+                    ),
+                    endActionPane: ActionPane(
+                      motion: const ScrollMotion(),
+                      extentRatio: 0.2,
+                      children: [
+                        SlidableAction(
+                          onPressed: (context) async {
+                            await ref
+                                .read(expensesProvider.notifier)
+                                .deleteExpense(id: state.expenses[index].id);
+                            ref.invalidate(expensesProvider);
+                          },
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
+                          icon: Icons.delete,
+                          label: 'Eliminar',
+                        ),
+                      ],
+                    ),
+                    child: ExpenseWidget(
+                      expenseName: state.expenses[index].name,
+                      amount: state.expenses[index].amount,
+                      bankName: state.expenses[index].bank?.name,
+                      isFixed: state.expenses[index].isFixed,
+                      payMethod: state.expenses[index].payMethod.name,
+                    ),
                   );
                 },
               ),
