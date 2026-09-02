f495c497e191e018114d246522fb1711e0af35a9
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-02 12:57:00 -0300

    feat(fixed_term_deposit): update date_utils and rename to due_date_utils

--- lib/features/fixed_term_deposit/presentation/utils/due_date_utils.dart
+++ lib/features/fixed_term_deposit/presentation/utils/due_date_utils.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 
 Color getDueDateColor(int daysBeforeDueDate) {
   return switch (daysBeforeDueDate) {
+    == 0 => Colors.grey,
     <= 3 => Colors.red,
     <= 6 => Colors.deepOrangeAccent,
     _ => Colors.green,
+  };
+}
+
+String getDueDateMessage(int days) {
+  return switch (days) {
+    == 1 => '$days día',
+    _ => '$days días',
   };
 }
