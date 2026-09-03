bfb4a676b1805b053590979f792c53f5f383fec6
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-03 11:51:14 -0300

    feat(money_pockets): add Money Pocket entity

---
+++ lib/features/money_pockets/domain/entities/money_pocket_entity.dart
@@ -0,0 +1,11 @@
+class MoneyPocketEntity {
+  const MoneyPocketEntity({
+    required this.id,
+    required this.name,
+    required this.amount,
+  });
+
+  final int id;
+  final String name;
+  final double amount;
+}
