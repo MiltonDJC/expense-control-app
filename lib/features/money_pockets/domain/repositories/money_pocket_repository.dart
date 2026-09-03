954609f6ff279acceb521e166ffc1c45f5ebd98f
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-03 12:21:34 -0300

    feat(money_pockets): update contract method

--- lib/features/money_pockets/domain/repositories/money_pocket_repository.dart
+++ lib/features/money_pockets/domain/repositories/money_pocket_repository.dart
@@ -3,7 +3,11 @@ import 'package:expense_control_app/features/money_pockets/domain/entities/money
 abstract class MoneyPocketRepository {
   Future<List<MoneyPocketEntity>> getAllMoneyPockets();
 
-  Future<void> updateMoneyPocket({required int id});
+  Future<void> updateMoneyPocket({
+    required int id,
+    String? name,
+    double? amount,
+  });
 
   Future<void> deleteMoneyPocket({required int id});
 }
