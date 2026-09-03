a51f8a31b774258c21ae065f6a77a387c73e4c19
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-03 14:22:22 -0300

    feat(money_pockets): update money pockets repository adding add money pocket contract

--- lib/features/money_pockets/domain/repositories/money_pockets_repository.dart
+++ lib/features/money_pockets/domain/repositories/money_pockets_repository.dart
@@ -3,6 +3,8 @@ import 'package:expense_control_app/features/money_pockets/domain/entities/money
 abstract class MoneyPocketsRepository {
   Future<List<MoneyPocketEntity>> getAllMoneyPockets();
 
+  Future<void> addMoneyPocket({required String name, required double amount});
+
   Future<void> updateMoneyPocket({
     required int id,
     String? name,
