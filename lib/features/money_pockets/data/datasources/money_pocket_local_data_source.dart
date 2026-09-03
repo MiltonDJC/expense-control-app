5d7929d7cf4604d2346ba45dd192ef65374b9e77
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-03 12:54:38 -0300

    feat(money_pocket): add money pocket local datasource

---
+++ lib/features/money_pockets/data/datasources/money_pocket_local_data_source.dart
@@ -0,0 +1,42 @@
+import 'package:drift/drift.dart';
+import 'package:expense_control_app/core/database/app_database.dart';
+import 'package:expense_control_app/features/money_pockets/data/models/money_pocket_model.dart';
+
+class MoneyPocketLocalDataSource {
+  MoneyPocketLocalDataSource({required this.appDatabase});
+
+  final AppDatabase appDatabase;
+
+  Future<List<MoneyPocketModel>> getAllFixedTermDeposits() async {
+    final moneyPocketRows = await appDatabase.managers.moneyPocket.get();
+    final moneyPocketModels = moneyPocketRows
+        .map(
+          (moneyPocketRow) => MoneyPocketModel(
+            id: moneyPocketRow.id,
+            name: moneyPocketRow.name,
+            amount: moneyPocketRow.amount,
+          ),
+        )
+        .toList();
+    return moneyPocketModels;
+  }
+
+  Future<void> updateMoneyPocket({
+    required int id,
+    String? name,
+    double? amount,
+  }) async {
+    await appDatabase.managers.moneyPocket
+        .filter((f) => f.id(id))
+        .update(
+          (o) => o(
+            name: Value.absentIfNull(name),
+            amount: Value.absentIfNull(amount),
+          ),
+        );
+  }
+
+  Future<void> deleteMoneyPocket({required int id}) async {
+    await appDatabase.managers.moneyPocket.filter((f) => f.id(id)).delete();
+  }
+}
