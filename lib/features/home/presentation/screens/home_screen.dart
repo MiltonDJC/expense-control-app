3bf057cff0a4f974c45d5a621af8067e30adc199
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-03 21:17:30 -0300

    feat(home): add padding to the body

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -67,7 +67,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12),
+        padding: const EdgeInsets.only(left: 12, top: 12, right: 12),
         child: screens[selectedIndex],
       ),
     );
