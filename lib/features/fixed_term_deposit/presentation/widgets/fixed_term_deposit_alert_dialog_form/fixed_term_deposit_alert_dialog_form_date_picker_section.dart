ed26725b0b22ee29950d33f76bff487f4387388a
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-08 21:03:36 -0300

    feat(fixed_term_deposit): add comment to update the widget later

--- lib/features/fixed_term_deposit/presentation/widgets/fixed_term_deposit_alert_dialog_form/fixed_term_deposit_alert_dialog_form_date_picker_section.dart
+++ lib/features/fixed_term_deposit/presentation/widgets/fixed_term_deposit_alert_dialog_form/fixed_term_deposit_alert_dialog_form_date_picker_section.dart
@@ -12,6 +12,10 @@ class FixedTermDepositAlertDialogFormDatePickerSection extends StatefulWidget {
   final String title;
   final String? hintText;
   final Function(DateTime?) datePicked;
+
+  // TODO: Revisar si cambiar el 'String?' de la función validator por DateTime?
+  // * Relacionado con el archivo: lib\features\fixed_term_deposit\presentation\widgets\fixed_term_deposit_alert_dialog_form\fixed_term_deposit_alert_dialog_form_update_widget.dart
+
   final String? Function(String?)? validator;
 
   @override
