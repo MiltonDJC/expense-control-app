0ae3c62f40998c2f51fcadd8db2a6e375050097c
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-10 23:39:33 -0300

    feat(expenses): update AddExpenseUseCase for using both money pocket feature and expenses feature

--- lib/features/expenses/domain/use_cases/add_expense_use_case.dart
+++ lib/features/expenses/domain/use_cases/add_expense_use_case.dart
@@ -1,11 +1,16 @@
 import 'package:expense_control_app/features/expenses/domain/enums/bank.dart';
 import 'package:expense_control_app/features/expenses/domain/enums/pay_method.dart';
 import 'package:expense_control_app/features/expenses/domain/repositories/expenses_repository.dart';
+import 'package:expense_control_app/features/money_pockets/domain/repositories/money_pockets_repository.dart';
 
 class AddExpenseUseCase {
-  AddExpenseUseCase({required this.expensesRepository});
+  AddExpenseUseCase({
+    required this.expensesRepository,
+    required this.moneyPocketsRepository,
+  });
 
   final ExpensesRepository expensesRepository;
+  final MoneyPocketsRepository moneyPocketsRepository;
 
   Future<void> call({
     required String name,
@@ -13,11 +18,36 @@ class AddExpenseUseCase {
     required PayMethod payMethod,
     Bank? bank,
     required bool isFixed,
-  }) => expensesRepository.addExpense(
-    name: name,
-    amount: amount,
-    payMethod: payMethod,
-    bank: bank,
-    isFixed: isFixed,
-  );
+    int? moneyPocketId,
+  }) async {
+    if (moneyPocketId == null) {
+      await expensesRepository.addExpense(
+        name: name,
+        amount: amount,
+        payMethod: payMethod,
+        bank: bank,
+        isFixed: isFixed,
+        moneyPocketId: null,
+      );
+    } else {
+      final moneyPockets = await moneyPocketsRepository.getAllMoneyPockets();
+
+      final moneyPocket = moneyPockets.firstWhere(
+        (moneyPocket) => moneyPocket.id == moneyPocketId,
+      );
+
+      await expensesRepository.addExpense(
+        name: name,
+        amount: amount,
+        payMethod: payMethod,
+        bank: bank,
+        isFixed: isFixed,
+        moneyPocketId: moneyPocketId,
+      );
+      await moneyPocketsRepository.updateMoneyPocket(
+        id: moneyPocketId,
+        amount: moneyPocket.amount - amount,
+      );
+    }
+  }
 }
