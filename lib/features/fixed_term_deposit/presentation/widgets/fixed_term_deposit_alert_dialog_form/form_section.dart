e537e78d526872ca19f88ee8609735ed5eaff5e5
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-11 19:26:30 -0300

    feat(money_pockets): add keyboarType property

--- lib/features/fixed_term_deposit/presentation/widgets/fixed_term_deposit_alert_dialog_form/form_section.dart
+++ lib/features/fixed_term_deposit/presentation/widgets/fixed_term_deposit_alert_dialog_form/form_section.dart
@@ -7,12 +7,14 @@ class FormSection extends StatelessWidget {
     required this.hintText,
     required this.controller,
     required this.validator,
+    required this.keyboardType,
   });
 
   final String title;
   final String hintText;
   final TextEditingController controller;
   final String? Function(String?)? validator;
+  final TextInputType keyboardType;
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +31,7 @@ class FormSection extends StatelessWidget {
             validator: validator,
             autovalidateMode: AutovalidateMode.onUserInteractionIfError,
             controller: controller,
-            keyboardType: const TextInputType.numberWithOptions(decimal: true),
+            keyboardType: keyboardType,
             style: const TextStyle(fontSize: 18),
             decoration: InputDecoration(
               hintText: hintText,
