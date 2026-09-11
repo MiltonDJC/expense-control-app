b6e88a76fe9d31cbaba685c93de670b92ca01c84
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-11 13:48:56 -0300

    feat(core): update string formatter extension

--- lib/core/presentation/extensions/string_formatter.dart
+++ lib/core/presentation/extensions/string_formatter.dart
@@ -1,6 +1,10 @@
 extension StringFormatter on String {
   String get capitalize {
     if (this.isEmpty) return this;
-    return '${this[0].toUpperCase()}${this.substring(1)}';
+    return this
+        .split(' ')
+        .map((e) => e[0].toUpperCase() + e.substring(1))
+        .join(' ')
+        .toString();
   }
 }
