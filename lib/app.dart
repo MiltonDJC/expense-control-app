1873f007b77545af08930b48742a2645b672c9ab
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-13 10:14:56 -0300

    feat(app): update app adding theme config

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,19 @@
 import 'package:expense_control_app/features/home/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'core/theme/theme.dart';
+
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      home: const HomeScreen(),
     );
   }
 }
