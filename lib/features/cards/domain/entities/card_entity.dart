34d088fba490828ad2cbc4e51d567aa53391084a
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-16 17:33:29 -0300

    feat(cards): add both card entity and credit card type enum

---
+++ lib/features/cards/domain/entities/card_entity.dart
@@ -0,0 +1,18 @@
+import 'package:expense_control_app/domain/enums/bank.dart';
+import 'package:expense_control_app/features/cards/domain/enums/credit_card_type.dart';
+
+class CardEntity {
+  const CardEntity({
+    required this.cardName,
+    required this.creditCardType,
+    required this.bank,
+    required this.dueDate,
+    required this.closeDate,
+  });
+
+  final String cardName;
+  final CreditCardType creditCardType;
+  final Bank bank;
+  final DateTime dueDate;
+  final DateTime closeDate;
+}
---
+++ lib/features/cards/domain/enums/credit_card_type.dart
@@ -0,0 +1,1 @@
+enum CreditCardType { mastercard, visa }
