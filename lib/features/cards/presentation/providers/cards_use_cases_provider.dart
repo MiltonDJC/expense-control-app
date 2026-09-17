93bee92a766d499f6a56a16e4fdccfefe3e57468
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-16 23:12:56 -0300

    feat(cards): add addCreditCardUseCase provider

--- lib/features/cards/presentation/providers/cards_use_cases_provider.dart
+++ lib/features/cards/presentation/providers/cards_use_cases_provider.dart
@@ -1,3 +1,4 @@
+import 'package:expense_control_app/features/cards/domain/use_cases/add_credit_card_use_case.dart';
 import 'package:expense_control_app/features/cards/domain/use_cases/get_all_cards_use_case.dart';
 import 'package:expense_control_app/features/cards/presentation/providers/cards_repository_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -7,4 +8,10 @@ part 'cards_use_cases_provider.g.dart';
 GetAllCardsUseCase getAllCardsUseCase(Ref ref) {
   final repository = ref.read(cardsRepositoryProvider);
   return GetAllCardsUseCase(cardsRepository: repository);
+}
+
+@riverpod
+AddCreditCardUseCase addCreditCardUseCase(Ref ref) {
+  final repository = ref.read(cardsRepositoryProvider);
+  return AddCreditCardUseCase(cardsRepository: repository);
 }
