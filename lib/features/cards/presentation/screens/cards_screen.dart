3cd7c6f032de917825343a82133961a28a842c56
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-18 20:04:16 -0300

    feat(cards): replace scaffold with stack

--- lib/features/cards/presentation/screens/cards_screen.dart
+++ lib/features/cards/presentation/screens/cards_screen.dart
@@ -14,151 +14,166 @@ class CardsScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final cards = ref.watch(cardsProvider);
 
-    return Scaffold(
-      body: cards.when(
-        data: (state) => state.cards.isEmpty
-            ? const Center(
-                child: Text(
-                  'No hay tarjetas registradas por el momento...',
-                  style: TextStyle(fontSize: 26),
-                ),
-              )
-            : Padding(
-                padding: const EdgeInsets.only(bottom: 72, top: 18),
-                child: Center(
-                  child: GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 3,
-                          childAspectRatio: 1.6,
-                          mainAxisSpacing: 18,
-                          crossAxisSpacing: 12,
-                        ),
-                    itemCount: state.cards.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Builder(
-                        builder: (BuildContext innerContext) {
-                          return InkWell(
-                            onLongPress: () async {
-                              final RenderBox renderBox =
-                                  innerContext.findRenderObject() as RenderBox;
-                              final size = renderBox.size;
-                              final position = renderBox.localToGlobal(
-                                Offset.zero,
-                              );
+    return Stack(
+      children: [
+        cards.when(
+          data: (state) => state.cards.isEmpty
+              ? const Center(
+                  child: Text(
+                    'No hay tarjetas registradas por el momento...',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                )
+              : Padding(
+                  padding: const EdgeInsets.only(bottom: 72, top: 18),
+                  child: Center(
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            childAspectRatio: 1.6,
+                            mainAxisSpacing: 18,
+                            crossAxisSpacing: 12,
+                          ),
+                      itemCount: state.cards.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Builder(
+                          builder: (BuildContext innerContext) {
+                            return InkWell(
+                              onLongPress: () async {
+                                final RenderBox renderBox =
+                                    innerContext.findRenderObject()
+                                        as RenderBox;
+                                final size = renderBox.size;
+                                final position = renderBox.localToGlobal(
+                                  Offset.zero,
+                                );
 
-                              final centerX = position.dx + (size.width / 2.5);
-                              final centerY = position.dy + (size.height / 2.5);
+                                final centerX =
+                                    position.dx + (size.width / 2.5);
+                                final centerY =
+                                    position.dy + (size.height / 2.5);
 
-                              await showMenu(
-                                context: context,
-                                position: RelativeRect.fromLTRB(
-                                  centerX,
-                                  centerY,
-                                  centerX,
-                                  centerY,
-                                ),
-                                items: [
-                                  const PopupMenuItem(
-                                    child: Icon(Icons.arrow_back),
+                                await showMenu(
+                                  context: context,
+                                  position: RelativeRect.fromLTRB(
+                                    centerX,
+                                    centerY,
+                                    centerX,
+                                    centerY,
                                   ),
-                                  PopupMenuItem(
-                                    child: const Text('Editar'),
-                                    onTap: () async => await showDialog(
-                                      context: context,
-                                      builder: (context) =>
-                                          CardAlertDialogFormUpdate(
-                                            id: state.cards[index].id,
-                                            creditCardType: state
-                                                .cards[index]
-                                                .creditCardType,
-                                            bank: state.cards[index].bank,
-                                            dueDate: state.cards[index].dueDate,
-                                            closeDate:
-                                                state.cards[index].closeDate,
-                                          ),
+                                  items: [
+                                    const PopupMenuItem(
+                                      child: Icon(Icons.arrow_back),
                                     ),
-                                  ),
-                                  PopupMenuItem(
-                                    child: const Text(
-                                      'Eliminar',
-                                      style: TextStyle(color: Colors.red),
+                                    PopupMenuItem(
+                                      child: const Text('Editar'),
+                                      onTap: () async => await showDialog(
+                                        context: context,
+                                        builder: (context) =>
+                                            CardAlertDialogFormUpdate(
+                                              id: state.cards[index].id,
+                                              creditCardType: state
+                                                  .cards[index]
+                                                  .creditCardType,
+                                              bank: state.cards[index].bank,
+                                              dueDate:
+                                                  state.cards[index].dueDate,
+                                              closeDate:
+                                                  state.cards[index].closeDate,
+                                            ),
+                                      ),
                                     ),
-                                    onTap: () async => await showDialog(
-                                      context: context,
-                                      builder: (context) => AlertDialog(
-                                        content: const SizedBox(
-                                          width: 288,
-                                          height: 100,
-                                          child: Center(
-                                            child: Text(
-                                              '¿Segura quiere eliminar la tarjeta?',
-                                              textAlign: TextAlign.center,
-                                              style: TextStyle(
-                                                fontSize: 24,
-                                                fontWeight: FontWeight.w600,
+                                    PopupMenuItem(
+                                      child: const Text(
+                                        'Eliminar',
+                                        style: TextStyle(color: Colors.red),
+                                      ),
+                                      onTap: () async => await showDialog(
+                                        context: context,
+                                        builder: (context) => AlertDialog(
+                                          content: const SizedBox(
+                                            width: 288,
+                                            height: 100,
+                                            child: Center(
+                                              child: Text(
+                                                '¿Segura quiere eliminar la tarjeta?',
+                                                textAlign: TextAlign.center,
+                                                style: TextStyle(
+                                                  fontSize: 24,
+                                                  fontWeight: FontWeight.w600,
+                                                ),
                                               ),
                                             ),
                                           ),
+                                          actions: [
+                                            ActionButtonWidget(
+                                              onPressed: () =>
+                                                  Navigator.pop(context),
+                                              text: 'Cancelar',
+                                            ),
+                                            ActionButtonWidget(
+                                              onPressed: () async {
+                                                await ref
+                                                    .read(
+                                                      cardsProvider.notifier,
+                                                    )
+                                                    .deleteCard(
+                                                      id: state.cards[index].id,
+                                                    );
+                                                if (context.mounted) {
+                                                  Navigator.pop(context);
+                                                }
+                                              },
+                                              text: 'Confirmar',
+                                            ),
+                                          ],
                                         ),
-                                        actions: [
-                                          ActionButtonWidget(
-                                            onPressed: () =>
-                                                Navigator.pop(context),
-                                            text: 'Cancelar',
-                                          ),
-                                          ActionButtonWidget(
-                                            onPressed: () async {
-                                              await ref
-                                                  .read(cardsProvider.notifier)
-                                                  .deleteCard(
-                                                    id: state.cards[index].id,
-                                                  );
-                                              if (context.mounted) {
-                                                Navigator.pop(context);
-                                              }
-                                            },
-                                            text: 'Confirmar',
-                                          ),
-                                        ],
                                       ),
                                     ),
-                                  ),
-                                ],
-                              );
-                            },
-                            child: CreditCardWidget(
-                              creditCardType: state.cards[index].creditCardType,
-                              bank: state.cards[index].bank,
-                              dueDate: state.cards[index].dueDate,
-                              closeDate: state.cards[index].closeDate,
-                              cardColor: Colors.green,
-                            ),
-                          );
-                        },
-                      );
-                    },
+                                  ],
+                                );
+                              },
+                              child: CreditCardWidget(
+                                creditCardType:
+                                    state.cards[index].creditCardType,
+                                bank: state.cards[index].bank,
+                                dueDate: state.cards[index].dueDate,
+                                closeDate: state.cards[index].closeDate,
+                                cardColor: Colors.green,
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
                   ),
                 ),
-              ),
-        error: (error, stackTrace) =>
-            Center(child: Text('Ha ocurrido un error: ${error.toString()}')),
-        loading: () => const Center(child: CircularProgressIndicator()),
-      ),
-      floatingActionButton: CustomFloatingActionButton(
-        title: 'Agregar tarjeta',
-        tooltip: 'Agregar nueva tarjeta',
-        icon: Icons.add,
-        onPressed: () async {
-          await showDialog(
-            context: context,
-            builder: (context) {
-              return const CardAlertDialogFormAdd();
-            },
-          );
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          error: (error, stackTrace) =>
+              Center(child: Text('Ha ocurrido un error: ${error.toString()}')),
+          loading: () => const Center(child: CircularProgressIndicator()),
+        ),
+        Positioned(
+          bottom: 16,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: CustomFloatingActionButton(
+              title: 'Agregar tarjeta',
+              tooltip: 'Agregar nueva tarjeta',
+              icon: Icons.add,
+              onPressed: () async {
+                await showDialog(
+                  context: context,
+                  builder: (context) {
+                    return const CardAlertDialogFormAdd();
+                  },
+                );
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
