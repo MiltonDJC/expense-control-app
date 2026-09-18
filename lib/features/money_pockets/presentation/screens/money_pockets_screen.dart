66284d636a1026a0d625f7e16fcbc15426dc7794
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-18 20:00:41 -0300

    feat(money_pockets): replace scaffold with stack

--- lib/features/money_pockets/presentation/screens/money_pockets_screen.dart
+++ lib/features/money_pockets/presentation/screens/money_pockets_screen.dart
@@ -20,157 +20,167 @@ class _MoneyPocketsScreenState extends ConsumerState<MoneyPocketsScreen> {
   Widget build(BuildContext context) {
     final moneyPockets = ref.watch(moneyPocketsProvider);
 
-    return Scaffold(
-      body: moneyPockets.when(
-        data: (state) => state.moneyPockets.isEmpty
-            ? const Center(
-                child: Text(
-                  'No hay bolsillos registrados por el momento...',
-                  style: TextStyle(fontSize: 26),
-                ),
-              )
-            : Padding(
-                padding: const EdgeInsets.only(bottom: 72),
-                child: ListView.builder(
-                  itemCount: state.moneyPockets.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 16),
-                      child: Slidable(
-                        startActionPane: ActionPane(
-                          motion: const ScrollMotion(),
-                          extentRatio: 0.2,
-                          children: [
-                            CustomSlidableAction(
-                              onPressed: (context) async {
-                                await showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return MoneyPocketUpdateForm(
-                                      id: state.moneyPockets[index].id,
-                                      name: state.moneyPockets[index].name,
-                                      amount: state.moneyPockets[index].amount,
-                                    );
-                                  },
-                                );
-                              },
-                              backgroundColor: Colors.blue,
-                              foregroundColor: Colors.white,
-                              child: const Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.edit,
-                                    color: Colors.white,
-                                    size: 28,
-                                  ),
-                                  SizedBox(height: 4),
-                                  Text(
-                                    'Editar',
-                                    style: TextStyle(
+    return Stack(
+      children: [
+        moneyPockets.when(
+          data: (state) => state.moneyPockets.isEmpty
+              ? const Center(
+                  child: Text(
+                    'No hay bolsillos registrados por el momento...',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                )
+              : Padding(
+                  padding: const EdgeInsets.only(bottom: 72),
+                  child: ListView.builder(
+                    itemCount: state.moneyPockets.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 16),
+                        child: Slidable(
+                          startActionPane: ActionPane(
+                            motion: const ScrollMotion(),
+                            extentRatio: 0.2,
+                            children: [
+                              CustomSlidableAction(
+                                onPressed: (context) async {
+                                  await showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return MoneyPocketUpdateForm(
+                                        id: state.moneyPockets[index].id,
+                                        name: state.moneyPockets[index].name,
+                                        amount:
+                                            state.moneyPockets[index].amount,
+                                      );
+                                    },
+                                  );
+                                },
+                                backgroundColor: Colors.blue,
+                                foregroundColor: Colors.white,
+                                child: const Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.edit,
                                       color: Colors.white,
-                                      fontSize: 20,
+                                      size: 28,
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        endActionPane: ActionPane(
-                          motion: const ScrollMotion(),
-                          extentRatio: 0.2,
-                          children: [
-                            CustomSlidableAction(
-                              onPressed: (context) async {
-                                await showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return AlertDialog(
-                                      title: const Center(
-                                        child: Text(
-                                          '¿Segura que quiere eliminar este bolsillo?',
-                                        ),
+                                    SizedBox(height: 4),
+                                    Text(
+                                      'Editar',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
                                       ),
-                                      actions: [
-                                        ActionButtonWidget(
-                                          onPressed: () async {
-                                            Navigator.pop(context);
-                                          },
-                                          text: 'Cancelar',
-                                        ),
-                                        ActionButtonWidget(
-                                          onPressed: () async {
-                                            await ref
-                                                .read(
-                                                  moneyPocketsProvider.notifier,
-                                                )
-                                                .deleteMoneyPocket(
-                                                  id: state
-                                                      .moneyPockets[index]
-                                                      .id,
-                                                );
-                                            if (context.mounted) {
-                                              Navigator.pop(context);
-                                            }
-                                          },
-                                          text: 'Confirmar',
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              },
-                              backgroundColor: Colors.red,
-                              foregroundColor: Colors.white,
-                              child: const Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.delete,
-                                    color: Colors.white,
-                                    size: 28,
-                                  ),
-                                  SizedBox(height: 4),
-                                  Text(
-                                    'Eliminar',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
+                          endActionPane: ActionPane(
+                            motion: const ScrollMotion(),
+                            extentRatio: 0.2,
+                            children: [
+                              CustomSlidableAction(
+                                onPressed: (context) async {
+                                  await showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Center(
+                                          child: Text(
+                                            '¿Segura que quiere eliminar este bolsillo?',
+                                          ),
+                                        ),
+                                        actions: [
+                                          ActionButtonWidget(
+                                            onPressed: () async {
+                                              Navigator.pop(context);
+                                            },
+                                            text: 'Cancelar',
+                                          ),
+                                          ActionButtonWidget(
+                                            onPressed: () async {
+                                              await ref
+                                                  .read(
+                                                    moneyPocketsProvider
+                                                        .notifier,
+                                                  )
+                                                  .deleteMoneyPocket(
+                                                    id: state
+                                                        .moneyPockets[index]
+                                                        .id,
+                                                  );
+                                              if (context.mounted) {
+                                                Navigator.pop(context);
+                                              }
+                                            },
+                                            text: 'Confirmar',
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                                backgroundColor: Colors.red,
+                                foregroundColor: Colors.white,
+                                child: const Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.delete,
+                                      color: Colors.white,
+                                      size: 28,
+                                    ),
+                                    SizedBox(height: 4),
+                                    Text(
+                                      'Eliminar',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          child: MoneyPocket(
+                            name: state.moneyPockets[index].name,
+                            amount: state.moneyPockets[index].amount,
+                          ),
                         ),
-                        child: MoneyPocket(
-                          name: state.moneyPockets[index].name,
-                          amount: state.moneyPockets[index].amount,
-                        ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 ),
-              ),
-        error: (error, stackTrace) =>
-            Center(child: Text('Hubo un error: ${error.toString()}')),
-        loading: () => const Center(child: CircularProgressIndicator()),
-      ),
-      floatingActionButton: CustomFloatingActionButton(
-        onPressed: () async {
-          await showDialog(
-            context: context,
-            builder: (context) {
-              return const MoneyPocketAddForm();
-            },
-          );
-        },
-        title: 'Agregar bolsillo',
-        icon: Icons.add,
-        tooltip: 'Agregar nuevo bolsillo',
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          error: (error, stackTrace) =>
+              Center(child: Text('Hubo un error: ${error.toString()}')),
+          loading: () => const Center(child: CircularProgressIndicator()),
+        ),
+        Positioned(
+          bottom: 16,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: CustomFloatingActionButton(
+              onPressed: () async {
+                await showDialog(
+                  context: context,
+                  builder: (context) {
+                    return const MoneyPocketAddForm();
+                  },
+                );
+              },
+              title: 'Agregar bolsillo',
+              icon: Icons.add,
+              tooltip: 'Agregar nuevo bolsillo',
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
