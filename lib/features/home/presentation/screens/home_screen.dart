7319ccaf1060e077261a30ac933bfb3cfd342474
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-18 19:50:08 -0300

    feat(cards): add safe area in home screen widget

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -26,73 +26,80 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size(double.infinity, 80),
-        child: NavigationBar(
-          shadowColor: Colors.black,
-          elevation: 6,
-          selectedIndex: selectedIndex,
-          onDestinationSelected: (value) =>
-              setState(() => selectedIndex = value),
-          indicatorColor: Theme.of(context).focusColor,
-          labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
-          labelTextStyle: const WidgetStatePropertyAll(
-            TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: PreferredSize(
+          preferredSize: const Size(double.infinity, 80),
+          child: NavigationBar(
+            shadowColor: Colors.black,
+            elevation: 6,
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (value) =>
+                setState(() => selectedIndex = value),
+            indicatorColor: Theme.of(context).focusColor,
+            labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+            labelTextStyle: const WidgetStatePropertyAll(
+              TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+            destinations: const [
+              NavigationDestination(
+                icon: Icon(Icons.account_balance_wallet_outlined, size: 26),
+                selectedIcon: Icon(
+                  Icons.account_balance_wallet,
+                  color: Colors.black,
+                  size: 30,
+                ),
+                label: 'Balance',
+                tooltip: 'Mi balance',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.savings_outlined, size: 26),
+                selectedIcon: Icon(
+                  Icons.savings,
+                  color: Colors.black,
+                  size: 30,
+                ),
+                label: 'Bolsillos',
+                tooltip: 'Mis bolsillos',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.money_off_csred_outlined, size: 26),
+                selectedIcon: Icon(
+                  Icons.money_off_csred,
+                  color: Colors.black,
+                  size: 30,
+                ),
+                label: 'Gastos',
+                tooltip: 'Mis gastos',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.account_balance_outlined, size: 26),
+                selectedIcon: Icon(
+                  Icons.account_balance,
+                  color: Colors.black,
+                  size: 30,
+                ),
+                label: 'Plazos Fijos',
+                tooltip: 'Mis plazos fijos',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.credit_card_outlined, size: 26),
+                selectedIcon: Icon(
+                  Icons.credit_card,
+                  color: Colors.black,
+                  size: 30,
+                ),
+                label: 'Tarjetas',
+                tooltip: 'Mis tarjetas',
+              ),
+            ],
           ),
-          destinations: const [
-            NavigationDestination(
-              icon: Icon(Icons.account_balance_wallet_outlined, size: 26),
-              selectedIcon: Icon(
-                Icons.account_balance_wallet,
-                color: Colors.black,
-                size: 30,
-              ),
-              label: 'Balance',
-              tooltip: 'Mi balance',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.savings_outlined, size: 26),
-              selectedIcon: Icon(Icons.savings, color: Colors.black, size: 30),
-              label: 'Bolsillos',
-              tooltip: 'Mis bolsillos',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.money_off_csred_outlined, size: 26),
-              selectedIcon: Icon(
-                Icons.money_off_csred,
-                color: Colors.black,
-                size: 30,
-              ),
-              label: 'Gastos',
-              tooltip: 'Mis gastos',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.account_balance_outlined, size: 26),
-              selectedIcon: Icon(
-                Icons.account_balance,
-                color: Colors.black,
-                size: 30,
-              ),
-              label: 'Plazos Fijos',
-              tooltip: 'Mis plazos fijos',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.credit_card_outlined, size: 26),
-              selectedIcon: Icon(
-                Icons.credit_card,
-                color: Colors.black,
-                size: 30,
-              ),
-              label: 'Tarjetas',
-              tooltip: 'Mis tarjetas',
-            ),
-          ],
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 12, top: 4, right: 12),
-        child: screens[selectedIndex],
+        body: Padding(
+          padding: const EdgeInsets.only(left: 12, top: 4, right: 12),
+          child: screens[selectedIndex],
+        ),
       ),
     );
   }
