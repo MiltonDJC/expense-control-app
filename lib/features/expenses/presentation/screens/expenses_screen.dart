4513f023ac0dd43186155c1ead06652a10f855c2
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-18 19:57:02 -0300

    feat(expenses): replace scaffold with stack

--- lib/features/expenses/presentation/screens/expenses_screen.dart
+++ lib/features/expenses/presentation/screens/expenses_screen.dart
@@ -15,156 +15,169 @@ class ExpensesScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final expenses = ref.watch(expensesProvider);
 
-    return Scaffold(
-      body: expenses.when(
-        data: (state) => state.expenses.isEmpty
-            ? const Center(
-                child: Text(
-                  'No hay gastos registrados por el momento...',
-                  style: TextStyle(fontSize: 26),
-                ),
-              )
-            : Padding(
-                padding: const EdgeInsets.only(bottom: 72),
-                child: ListView.builder(
-                  itemCount: state.expenses.length,
-                  itemBuilder: (context, index) {
-                    return Slidable(
-                      key: Key(state.expenses[index].id.toString()),
-                      startActionPane: ActionPane(
-                        motion: const ScrollMotion(),
-                        extentRatio: 0.2,
-                        children: [
-                          CustomSlidableAction(
-                            onPressed: (context) async {
-                              await showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return ExpenseAlertDialogFormUpdateWidget(
-                                    id: state.expenses[index].id,
-                                    expenseName: state.expenses[index].name,
-                                    amount: state.expenses[index].amount,
-                                    bankName: state.expenses[index].bank,
-                                    isFixed: state.expenses[index].isFixed,
-                                    payMethod: state.expenses[index].payMethod,
-                                  );
-                                },
-                              );
-                            },
-                            backgroundColor: Colors.blue,
-                            foregroundColor: Colors.white,
-                            child: const Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(Icons.edit, color: Colors.white, size: 28),
-                                SizedBox(height: 4),
-                                Text(
-                                  'Editar',
-                                  style: TextStyle(
+    return Stack(
+      children: [
+        expenses.when(
+          data: (state) => state.expenses.isEmpty
+              ? const Center(
+                  child: Text(
+                    'No hay gastos registrados por el momento...',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                )
+              : Padding(
+                  padding: const EdgeInsets.only(bottom: 72),
+                  child: ListView.builder(
+                    itemCount: state.expenses.length,
+                    itemBuilder: (context, index) {
+                      return Slidable(
+                        key: Key(state.expenses[index].id.toString()),
+                        startActionPane: ActionPane(
+                          motion: const ScrollMotion(),
+                          extentRatio: 0.2,
+                          children: [
+                            CustomSlidableAction(
+                              onPressed: (context) async {
+                                await showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return ExpenseAlertDialogFormUpdateWidget(
+                                      id: state.expenses[index].id,
+                                      expenseName: state.expenses[index].name,
+                                      amount: state.expenses[index].amount,
+                                      bankName: state.expenses[index].bank,
+                                      isFixed: state.expenses[index].isFixed,
+                                      payMethod:
+                                          state.expenses[index].payMethod,
+                                    );
+                                  },
+                                );
+                              },
+                              backgroundColor: Colors.blue,
+                              foregroundColor: Colors.white,
+                              child: const Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.edit,
                                     color: Colors.white,
-                                    fontSize: 20,
+                                    size: 28,
                                   ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      endActionPane: ActionPane(
-                        motion: const ScrollMotion(),
-                        extentRatio: 0.2,
-                        children: [
-                          CustomSlidableAction(
-                            onPressed: (context) async {
-                              await showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return AlertDialog(
-                                    title: const Center(
-                                      child: Text(
-                                        '¿Segura que quiere eliminar este gasto?',
-                                      ),
+                                  SizedBox(height: 4),
+                                  Text(
+                                    'Editar',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
                                     ),
-                                    actions: [
-                                      ActionButtonWidget(
-                                        onPressed: () async {
-                                          Navigator.pop(context);
-                                        },
-                                        text: 'Cancelar',
-                                      ),
-                                      ActionButtonWidget(
-                                        onPressed: () async {
-                                          await ref
-                                              .read(expensesProvider.notifier)
-                                              .deleteExpense(
-                                                id: state.expenses[index].id,
-                                              );
-                                          ref.invalidate(expensesProvider);
-                                          if (context.mounted) {
-                                            Navigator.pop(context);
-                                          }
-                                        },
-                                        text: 'Confirmar',
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                            },
-                            backgroundColor: Colors.red,
-                            foregroundColor: Colors.white,
-                            child: const Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(
-                                  Icons.delete,
-                                  color: Colors.white,
-                                  size: 28,
-                                ),
-                                SizedBox(height: 4),
-                                Text(
-                                  'Eliminar',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      child: ExpenseInformationWidget(
-                        expenseName: state.expenses[index].name,
-                        amount: state.expenses[index].amount,
-                        bankName: state.expenses[index].bank,
-                        isFixed: state.expenses[index].isFixed,
-                        payMethod: state.expenses[index].payMethod,
-                        moneyPocketId: state.expenses[index].moneyPocketId,
-                        createdDate: state.expenses[index].createdDate,
-                      ),
-                    );
-                  },
+                          ],
+                        ),
+                        endActionPane: ActionPane(
+                          motion: const ScrollMotion(),
+                          extentRatio: 0.2,
+                          children: [
+                            CustomSlidableAction(
+                              onPressed: (context) async {
+                                await showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AlertDialog(
+                                      title: const Center(
+                                        child: Text(
+                                          '¿Segura que quiere eliminar este gasto?',
+                                        ),
+                                      ),
+                                      actions: [
+                                        ActionButtonWidget(
+                                          onPressed: () async {
+                                            Navigator.pop(context);
+                                          },
+                                          text: 'Cancelar',
+                                        ),
+                                        ActionButtonWidget(
+                                          onPressed: () async {
+                                            await ref
+                                                .read(expensesProvider.notifier)
+                                                .deleteExpense(
+                                                  id: state.expenses[index].id,
+                                                );
+                                            ref.invalidate(expensesProvider);
+                                            if (context.mounted) {
+                                              Navigator.pop(context);
+                                            }
+                                          },
+                                          text: 'Confirmar',
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
+                              backgroundColor: Colors.red,
+                              foregroundColor: Colors.white,
+                              child: const Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.delete,
+                                    color: Colors.white,
+                                    size: 28,
+                                  ),
+                                  SizedBox(height: 4),
+                                  Text(
+                                    'Eliminar',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        child: ExpenseInformationWidget(
+                          expenseName: state.expenses[index].name,
+                          amount: state.expenses[index].amount,
+                          bankName: state.expenses[index].bank,
+                          isFixed: state.expenses[index].isFixed,
+                          payMethod: state.expenses[index].payMethod,
+                          moneyPocketId: state.expenses[index].moneyPocketId,
+                          createdDate: state.expenses[index].createdDate,
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
-        error: (error, stackTrace) =>
-            Center(child: Text('Ha ocurrido un error: ${error.toString()}')),
-        loading: () => const Center(child: CircularProgressIndicator()),
-      ),
-      floatingActionButton: CustomFloatingActionButton(
-        onPressed: () async {
-          await showDialog(
-            context: context,
-            builder: (context) {
-              return const ExpenseAlertDialogFormAddWidget();
-            },
-          );
-        },
-        tooltip: 'Agregar nuevo gasto',
-        title: 'Agregar gasto',
-        icon: Icons.add,
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          error: (error, stackTrace) =>
+              Center(child: Text('Ha ocurrido un error: ${error.toString()}')),
+          loading: () => const Center(child: CircularProgressIndicator()),
+        ),
+        Positioned(
+          bottom: 16,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: CustomFloatingActionButton(
+              onPressed: () async {
+                await showDialog(
+                  context: context,
+                  builder: (context) {
+                    return const ExpenseAlertDialogFormAddWidget();
+                  },
+                );
+              },
+              tooltip: 'Agregar nuevo gasto',
+              title: 'Agregar gasto',
+              icon: Icons.add,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
