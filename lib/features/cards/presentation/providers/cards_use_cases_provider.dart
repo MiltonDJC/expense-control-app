0d205b696781e733c42f4e85a22befe78e4707da
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-18 18:03:26 -0300

    feat(cards): add delete card use case provider

--- lib/features/cards/presentation/providers/cards_use_cases_provider.dart
+++ lib/features/cards/presentation/providers/cards_use_cases_provider.dart
@@ -1,4 +1,5 @@
 import 'package:expense_control_app/features/cards/domain/use_cases/add_credit_card_use_case.dart';
+import 'package:expense_control_app/features/cards/domain/use_cases/delete_card_use_case.dart';
 import 'package:expense_control_app/features/cards/domain/use_cases/get_all_cards_use_case.dart';
 import 'package:expense_control_app/features/cards/presentation/providers/cards_repository_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -14,4 +15,10 @@ GetAllCardsUseCase getAllCardsUseCase(Ref ref) {
 AddCreditCardUseCase addCreditCardUseCase(Ref ref) {
   final repository = ref.read(cardsRepositoryProvider);
   return AddCreditCardUseCase(cardsRepository: repository);
+}
+
+@riverpod
+DeleteCardUseCase deleteCardUseCase(Ref ref) {
+  final repository = ref.read(cardsRepositoryProvider);
+  return DeleteCardUseCase(cardsRepository: repository);
 }
