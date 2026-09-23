173b5eaa0a80fd849dcad35c715b1a923ff2db61
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-23 12:46:31 -0300

    feat(core): update snackbar text font size

--- lib/core/presentation/utils/app_snack_bar.dart
+++ lib/core/presentation/utils/app_snack_bar.dart
@@ -4,7 +4,7 @@ class AppSnackBar {
   static void show(BuildContext context, String message) =>
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text(message, style: const TextStyle(fontSize: 24)),
+          content: Text(message, style: const TextStyle(fontSize: 18)),
           showCloseIcon: true,
           duration: const Duration(milliseconds: 1500),
         ),
