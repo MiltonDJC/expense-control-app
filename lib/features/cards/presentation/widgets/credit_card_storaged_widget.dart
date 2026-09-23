d7037c5fb78d92582ad792d611d1d2ac625a3e64
MiltonDJC <miltonaxe2001@gmail.com>
2026-09-23 14:21:40 -0300

    feat(cards): add credit card storaged widget

---
+++ lib/features/cards/presentation/widgets/credit_card_storaged_widget.dart
@@ -0,0 +1,130 @@
+import 'package:expense_control_app/core/presentation/utils/format_date_utils.dart';
+import 'package:expense_control_app/domain/enums/bank.dart';
+import 'package:expense_control_app/features/cards/domain/enums/credit_card_type.dart';
+import 'package:expense_control_app/features/expenses/presentation/utils/bank_utils.dart';
+import 'package:flutter/material.dart';
+
+class CreditCardStoragedWidget extends StatelessWidget {
+  const CreditCardStoragedWidget({
+    super.key,
+    required this.creditCardType,
+    required this.bank,
+    required this.dueDate,
+    required this.closeDate,
+    required this.cardColor,
+  });
+
+  final CreditCardType creditCardType;
+  final Bank bank;
+  final DateTime dueDate;
+  final DateTime closeDate;
+  final Color cardColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final String formattedDueDate = formatDate(dueDate);
+    final String formattedCloseDate = formatDate(closeDate);
+
+    return Container(
+      width: 480,
+      height: 234,
+      padding: const EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        color: cardColor,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                getBankName(bank.name).toUpperCase(),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.2,
+                ),
+              ),
+              Text(
+                creditCardType.name.toUpperCase(),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 35,
+                height: 25,
+                decoration: BoxDecoration(
+                  color: Colors.amber[300],
+                  borderRadius: BorderRadius.circular(6),
+                  border: Border.all(color: Colors.amber[700]!, width: 1),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  '**** **** **** 3892',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    letterSpacing: 3.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Cierre:\n$formattedCloseDate',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    'Vencimiento:\n$formattedDueDate',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
